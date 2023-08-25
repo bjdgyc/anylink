da1d6c6c6db1741779e816521054a101ff89c2bd
bjdgyc <bjdgyc@163.com>
2023-08-25 13:56:04 +0800

    添加安全的header头

--- server/pkg/utils/secure_header.go
+++ server/pkg/utils/secure_header.go
@@ -7,9 +7,11 @@ func SetSecureHeader(w http.ResponseWriter) {
 	// Content-Length Date 默认已经存在
 	w.Header().Set("Server", "AnyLinkOpenSource")
 	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// w.Header().Set("Transfer-Encoding", "chunked")
+	w.Header().Set("X-Aggregate-Auth", "1")
+
 	w.Header().Set("Cache-Control", "no-store,no-cache")
 	w.Header().Set("Pragma", "no-cache")
-	w.Header().Set("Transfer-Encoding", "chunked")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("X-Frame-Options", "deny")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
@@ -17,11 +19,11 @@ func SetSecureHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'")
 	w.Header().Set("X-Permitted-Cross-Domain-Policies", "none")
 	w.Header().Set("Referrer-Policy", "no-referrer")
-	// w.Header().Set("Clear-Site-Data", "cache,cookies,storage")
 	w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
 	w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
 	w.Header().Set("Cross-Origin-Resource-Policy", "same-origin")
 	w.Header().Set("X-XSS-Protection", "0")
-	w.Header().Set("X-Aggregate-Auth", "1")
 	w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+
+	// w.Header().Set("Clear-Site-Data", "cache,cookies,storage")
 }
