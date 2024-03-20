11f39d0b785e919a82acc167f6ba4cac666b9bd3
bjdgyc <bjdgyc@163.com>
2024-03-20 14:23:56 +0800

    修复心跳时间

--- server/handler/link_tunnel.go
+++ server/handler/link_tunnel.go
@@ -181,7 +181,7 @@ func LinkTunnel(w http.ResponseWriter, r *http.Request) {
 	HttpSetHeader(w, "X-CSTP-Quarantine", "false")
 	HttpSetHeader(w, "X-CSTP-Disable-Always-On-VPN", "false")
 	HttpSetHeader(w, "X-CSTP-Client-Bypass-Protocol", "false")
-	HttpSetHeader(w, "X-CSTP-TCP-Keepalive", "true")
+	HttpSetHeader(w, "X-CSTP-TCP-Keepalive", "false")
 	// 设置域名拆分隧道（移动端不支持）
 	if mobile != "mobile" {
 		SetPostAuthXml(cSess.Group, w)
